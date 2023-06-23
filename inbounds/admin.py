a14c5bd0af668c16cfc1be98aecf3a64229d5117
Fortify Access <fortifyaccess@gmail.com>
2023-06-24 00:31:48 +0330

    some mode model amdins added and override their forms to have more flexiblity.

--- inbounds/admin.py
+++ inbounds/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from . import models
+from . import functions
 
 # Register your models here.
+class InboundUserInline(admin.TabularInline):
+    model = models.InboundUser
+
+
+class TlsInline(admin.StackedInline):
+    model = models.Tls
+
+    def get_formset(self, request, obj=None, **kwargs):
+        formset = super().get_formset(request, obj, **kwargs)
+        if obj:
+            formset.form.base_fields['server_name'].initial = obj.server.host
+            formset.form.base_fields['handshake_server'].initial = obj.server.host
+            formset.form.base_fields['private_key'].initial, formset.form.base_fields['public_key'].initial = functions.get_reality_keypair()
+            formset.form.base_fields['short_id'].initial = functions.get_short_id()
+        return formset
+
+
+@admin.register(models.Inbound)
+class InboundAdmin(admin.ModelAdmin):
+    inlines = (InboundUserInline, TlsInline)
