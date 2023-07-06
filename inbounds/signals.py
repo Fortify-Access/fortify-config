fa62a8e839b255cb5e37d63a99b0653a3d65df95
Fortify Access <fortifyaccess@gmail.com>
2023-07-07 00:26:12 +0330

    more bug fixes in signals and enable dark mode.

--- inbounds/signals.py
+++ inbounds/signals.py
@@ -10,12 +10,18 @@ from . import functions
 
 @receiver(post_save, sender=models.Inbound)
 def commit_inbound_to_singbox(sender, instance, created, **kwargs):
-    if created:
-        with open(settings.SING_BOX_CONF_PATH, 'r') as config:
+    with open(settings.SING_BOX_CONF_PATH, 'r') as config:
+        if created:
             config_dict = json.loads(config.read())
             config_dict['inbounds'].append(instance.to_dict())
-            config_dict = json.dumps(config_dict, indent=4)
-            open(settings.SING_BOX_CONF_PATH, 'w').write(config_dict)
+        else:
+            for index, inbound in enumerate(config_dict['inbounds']):
+                if inbound['tag'] == instance.tag:
+                    config_dict['inbounds'][index] = instance.to_dict()
+
+        config_dict = json.dumps(config_dict, indent=2)
+        open(settings.SING_BOX_CONF_PATH, 'w').write(config_dict)
+    print("Gooooooooooooh")
 
 
 @receiver(post_save, sender=models.InboundUser)
@@ -23,14 +29,14 @@ def commit_inbound_user_to_singbox(sender, instance, created, **kwargs):
     with open(settings.SING_BOX_CONF_PATH, 'r') as config:
         config_dict = json.loads(config.read())
         for index, inbound in enumerate(config_dict['inbounds']):
-            if created:
-                config_dict['inbounds'][index]['users'].append(instance.to_dict())
-            else:
-                if inbound['tag'] == instance.inbound.tag:
-                    config_dict['inbounds'][index]['users'][instance.uuid] = instance.to_dict
+            if inbound['tag'] == instance.inbound.tag:
+                if created:
+                    config_dict['inbounds'][index]['users'].append(instance.to_dict())
+                else:
+                    config_dict['inbounds'][index]['users'][instance.uuid] = instance.to_dict()
                     os.remove(settings.MEDIA_ROOT / 'qr_codes' / f'{instance.id}.png')
 
             functions.generate_qr_code(instance.connection_code, settings.MEDIA_ROOT / 'qr_codes' / f'{instance.id}.png')
 
-        config_dict = json.dumps(config_dict, indent=4)
+        config_dict = json.dumps(config_dict, indent=2)
         open(settings.SING_BOX_CONF_PATH, 'w').write(config_dict)
