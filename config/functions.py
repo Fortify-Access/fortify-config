5ae56731c8e5643e1f1b7b56b4e6f30000dbd425
Fortify Access <fortifyaccess@gmail.com>
2023-07-07 16:10:34 +0330

    nginx template generation for mappings and upstreams.

--- config/functions.py
+++ config/functions.py
@@ -1,7 +1,8 @@
 import subprocess
 from django.template.loader import render_to_string
 
-from inbounds import models
+from inbounds import models as inbound_models
+from . import models
 
 def restart_singbox():
     try:
@@ -12,8 +13,16 @@ def restart_singbox():
         return False
 
 def reload_nginx():
-    nginx_config = render_to_string('services/user_paths_template.conf', {'users': models.InboundUser.objects.all()})
-    with open('services/user_paths.conf', 'w') as config:
-        config.write(nginx_config)
-    
+    nginx_mappings = render_to_string(
+        'nginx_templates/mappings_template.conf',
+        {'users': inbound_models.InboundUser.objects.all(), 'host': models.Server.objects.get(is_local_server=True)})
+
+    with open('services/nginx_mappings.conf', 'w') as config:
+        config.write(nginx_mappings)
+
+    nginx_upstreams = render_to_string('nginx_templates/upstreams_template.conf',
+        {'users': inbound_models.InboundUser.objects.all()}, 'host': models.Server.objects.get(is_local_server=True)})
+    with open('services/nginx_upstreams.conf', 'w') as config:
+        config.write(nginx_upstreams)
+
     subprocess.run(['nginx', '-s', 'reload'])
